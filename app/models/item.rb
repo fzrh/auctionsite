6f18044c9662c532b83c6a2c7be51f6ee695e803
F <fwrizalman@gmail.com>
2016-03-29 19:49:11 +0800

    Add validations for item form

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,9 @@ class Item < ActiveRecord::Base
   belongs_to :user
   has_many :bids
 
+  validates :title, presence: true, length: { minimum: 10, maximum: 140 }
+  validates :base_price, presence: true, numericality: { only_integer: true }
+  validates :expire_date, presence: true
+
   default_scope -> { order('items.created_at DESC') }
 end
